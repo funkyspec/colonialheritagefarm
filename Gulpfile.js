454d83ab5003fa7d407f2811ac40a0f8c3418575
funkyspec <mrgresia@gmail.com>
2018-04-27 09:40:56 -0400

    Update gulpfile from bemskel

--- Gulpfile.js
+++ Gulpfile.js
@@ -37,9 +37,8 @@ const files = {
     'docs/css/bemskel/bemskel.css',
     'docs/css/bemskel/bemskel.min.css'
   ],
-  home: [ 'index.html', 'docs/css/custom.css' ]   //
+  home: [ 'index.html', 'docs/css/custom.css' ]
 }
-
 
 // Lint Task
 gulp.task('lint', function () {
@@ -89,7 +88,9 @@ gulp.task('build', function () {
 
 gulp.task('browser-sync', [ 'compileSass' ], function () {
   bs.init({
-    server: { baseDir: './docs' }
+    server: {
+      baseDir: './docs'
+    }
   })
 })
 
